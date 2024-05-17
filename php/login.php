dec15dc9f321ff1cd584d3cf5d7fd5526e9354df
WyattjkRichard <52966780+WyattjkRichard@users.noreply.github.com>
2024-05-16 21:06:51 -0400

    7 req access (#8)
    * Created Req access and login pages
    
    * Added GUI.drawio
    
    * Update GUI.drawio
    
    * Created Login, and request access pages
    added elevator buttons to index

---
+++ php/login.php
@@ -0,0 +1,28 @@
+<?php
+$submitted = !empty($_POST);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='assets/css/style.css' rel='stylesheet'>
+  <link rel="icon" href="assets/images/icon.png">
+  <title>Login Handler</title>
+</head>
+<body>
+  <div>
+    <a href="../index.html" style="text-decoration:none;">Home</a>
+    <a href="../about.html" style="text-decoration:none;">About</a>
+    <a href="../project_plan.html" style="text-decoration:none;">Project Plan</a>
+    <hr>
+  </div>
+
+  <p>Form Submitted? <?php echo (int) $submitted; ?></p>
+  <p>Your login info is</p>
+  <ul>
+    <li>Username: <?php echo $_POST['username']; ?></li>
+    <li>Password: <?php echo $_POST['password'];?></li>
+  </ul>
+</body>
+</html>
