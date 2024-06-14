1e352e4fa3dd404edd81e7d0c201ce7adfe57e15
WyattjkRichard <wyatt@rhouse.me>
2024-06-14 18:43:47 -0400

    Small php fixes

--- common/header.php
+++ common/header.php
@@ -5,13 +5,14 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="./index.php">Home</a>
-        <a class="nav-item nav-link" href="./about.php">About</a>
-        <a class="nav-item nav-link" href="./project_plan.php">Project Plan</a>
-        <a class="nav-item nav-link" href="./assignments.php">Assignments</a>
-        <a class="nav-item nav-link" href="./logbook_noah.php">Noah's Logbook</a>
-        <a class="nav-item nav-link" href="./logbook_wyatt.php">Wyatt's Logbook</a>
+        <a class="nav-item nav-link <?php if ($title=='Home') echo 'active';?>" href="./index.php">Home</a>
+        <a class="nav-item nav-link <?php if ($title=='About') echo 'active';?>" href="./about.php">About</a>
+        <a class="nav-item nav-link <?php if ($title=='Project Plan') echo 'active';?>" href="./project_plan.php">Project Plan</a>
+        <a class="nav-item nav-link <?php if ($title=='Assignments') echo 'active';?>" href="./assignments.php">Assignments</a>
+        <a class="nav-item nav-link <?php if ($title=="Noah's Logbook") echo 'active';?>" href="./logbook_noah.php">Noah's Logbook</a>
+        <a class="nav-item nav-link <?php if ($title=="Wyatt's Logbook") echo 'active';?>" href="./logbook_wyatt.php">Wyatt's Logbook</a>
       </div>
     </div>
   </nav>
 </header>
+
