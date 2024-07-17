55c2396da2fcb4747782b09ed6e78e8ec2b981fd
WyattjkRichard <wyatt@rhouse.me>
2024-07-17 19:09:06 -0400

    Continued work on diagnostics

--- diagnostics.php
+++ diagnostics.php
@@ -24,21 +24,28 @@ session_start();
                 if(isset($_SESSION['username'])){
                     echo <<<EOT
                     <ul class="nav nav-tabs">
-                        <li class="active"><a data-toggle="tab" href="#home">Floor History</a></li>
-                        <li><a data-toggle="tab" href="#menu1">User Activity</a></li>
-                        <li><a data-toggle="tab" href="#menu2">Requests Activity</a></li>
-                        
+                        <li class="active"><a data-toggle="tab" href="#fh_time">Floor History (Time)</a></li>
+                        <li><a data-toggle="tab" href="#fh_day">Floor History (Day)</a></li>
+                        <li><a data-toggle="tab" href="#rh_time">Requests History (Time)</a></li>
+                        <li><a data-toggle="tab" href="#rh_day">Requests History (Day)</a></li>
+                        <li><a data-toggle="tab" href="#pie">Pie Chart</a></li>
                     </ul>
 
                     <div class="tab-content">
-                        <div id="home" class="tab-pane fade in active">
-                            <canvas id="HOMEChart" style="width:100%;max-width:700px"></canvas>
+                        <div id="fh_time" class="tab-pane fade in active">
+                            <canvas id="fh_time_canv" style="width:100%;max-width:700px"></canvas>
                         </div>
-                        <div id="menu1" class="tab-pane fade">
-                            <canvas id="menu1Chart" style="width:100%;max-width:700px"></canvas>
+                        <div id="fh_day" class="tab-pane fade">
+                            <canvas id="fh_day_canv" style="width:100%;max-width:700px"></canvas>
                         </div>
-                        <div id="menu2" class="tab-pane fade">
-                            <canvas id="menu2Chart" style="width:100%;max-width:700px"></canvas>
+                        <div id="rh_time" class="tab-pane fade">
+                            <canvas id="rh_time_canv" style="width:100%;max-width:700px"></canvas>
+                        </div>
+                        <div id="rh_day" class="tab-pane fade">
+                            <canvas id="rh_day_canv" style="width:100%;max-width:700px"></canvas>
+                        </div>
+                        <div id="pie" class="tab-pane fade">
+                            <canvas id="pie_canv" style="width:100%;max-width:700px"></canvas>
                         </div>
                     </div>
                     EOT;
@@ -55,7 +62,7 @@ session_start();
             </div>
             <?php include "./common/footer.php"; ?>
         </div>
-        <script src="./js/diagnostics.js"></script>
+        <script src="./js/diagnostics.js?ver=0"></script>
         <?php include "./common/bottom.php"; ?>
     </body>
 </html>
--- js/diagnostics.js
+++ js/diagnostics.js
@@ -1,9 +1,34 @@
 // on load get all stats
-window.addEventListener("load", read_stats);
+window.addEventListener("load", readStats);
+
+// Global Variables
+const fh_counts = {
+    "hour": {
+        1: Array(24).fill(0),
+        2: Array(24).fill(0),
+        3: Array(24).fill(0)
+    },
+    "day":{
+        1: Array(7).fill(0),
+        2: Array(7).fill(0),
+        3: Array(7).fill(0),
+    }
+}
 
 
+const rh_counts = {
+    "hour":{
+       "Website": Array(24).fill(0),
+       "Voice": Array(24).fill(0)
+    },
+    "day":{
+        "Website": Array(7).fill(0),
+        "Voice": Array(24).fill(0)
+    }
+}
 
-function read_stats(){
+
+function readStats(){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -11,7 +36,9 @@ function read_stats(){
             let response = JSON.parse(xhttp.responseText);
             if(response.success){
                 console.log("Successfully got elevator history")
-                create_charts();
+                processData(response);
+                initializeFhTimeChart();
+                $('#fh_time_canv').data('initialized', true);
             }
             else{
                 console.log("Error getting elevator history")
@@ -23,36 +50,50 @@ function read_stats(){
     xhttp.send("function=read_stats");
 }
 
+function processData(data){
 
-function create_charts(){
-    var ctx = document.getElementById("HOMEChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
+    for ( id in data.floor_history) {
+        let day = new Date(data.floor_history[id]["Date"]).getDay();
+        let hour = data.floor_history[id]["Time"].substring(0,2);
+        let floor = data.floor_history[id]["Floor"];
+        fh_counts.hour[floor][parseInt(hour)]++;
+        fh_counts.day[floor][parseInt(day)]++;
+    }
+
+    for ( id in data.request_history) {
+        let day = new Date(data.request_history[id]["Date"]).getDay();
+        let hour = data.request_history[id]["Time"].substring(0,2);
+        let method = data.request_history[id]["Method"];
+        rh_counts.hour[method][parseInt(hour)]++;
+        rh_counts.day[method][parseInt(day)]++;
+    }
+}
+
+function initializeFhTimeChart() {
+    var ctx = document.getElementById("fh_time_canv").getContext("2d");
+    new Chart(ctx, {
+        type: "bar",
         data: {
-            labels: ["<  1","1 - 2","3 - 4","5 - 9","10 - 14","15 - 19","20 - 24","25 - 29","> - 29"],
+            labels: ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"],
             datasets: [{
-                label: "Webpage",
-                backgroundColor: "#caf270",
-                data: [12, 59, 5, 56, 58,12, 59, 87, 45],
-            }, {
-                label: "Voice",
-                backgroundColor: "#45c490",
-                data: [12, 59, 5, 56, 58,12, 59, 85, 23],
-            }, {
                 label: "Floor 1",
-                backgroundColor: "#008d93",
-                data: [12, 59, 5, 56, 58,12, 59, 65, 51],
+                backgroundColor: "#caf270",
+                data: fh_counts.hour[1],
             }, {
                 label: "Floor 2",
-                backgroundColor: "#2e5468",
-                data: [12, 59, 5, 56, 58, 12, 59, 12, 74],
+                backgroundColor: "#45c490",
+                data: fh_counts.hour[2],
+            }, {
+                label: "Floor 3",
+                backgroundColor: "#008d93",
+                data: fh_counts.hour[3],
             }],
         },
         options: {
             tooltips: {
                 displayColors: true,
                 callbacks:{
-                    mode: 'x',
+                    mode: "x",
                 },
             },
             scales: {
@@ -67,20 +108,175 @@ function create_charts(){
                     ticks: {
                         beginAtZero: true,
                     },
-                    type: 'linear',
+                    type: "linear",
                 }]
             },
             responsive: true,
             maintainAspectRatio: false,
-            legend: { position: 'bottom' },
+            legend: { position: "bottom" },
         }
     });
+}
 
+function initializeFhDayChart() {
+    var ctx = document.getElementById("fh_day_canv").getContext("2d");
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["SUN","MON","TUE","WED","THU","FRI","SAT"],
+            datasets: [{
+                label: "Floor 1",
+                backgroundColor: "#caf270",
+                data: fh_counts.day[1],
+            }, {
+                label: "Floor 2",
+                backgroundColor: "#45c490",
+                data: fh_counts.day[2],
+            }, {
+                label: "Floor 3",
+                backgroundColor: "#008d93",
+                data: fh_counts.day[3],
+            }],
+        },
+        options: {
+            tooltips: {
+                displayColors: true,
+                callbacks:{
+                    mode: "x",
+                },
+            },
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                    type: "linear",
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: { position: "bottom" },
+        }
+    });
+}
 
+function initializeRhTimeChart() {
+    var ctx = document.getElementById("rh_time_canv").getContext("2d");
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"],
+            datasets: [{
+                label: "Website",
+                backgroundColor: "#caf270",
+                data: rh_counts.hour["Website"],
+            }, {
+                label: "Voice",
+                backgroundColor: "#45c490",
+                data: rh_counts.hour["Voice"],
+            }/*, {
+                label: "Floor 1",
+                backgroundColor: "#008d93",
+                data: rh_counts.hour[""],
+            }, {
+                label: "Floor 2",
+                backgroundColor: "#2e5468",
+                data: rh_counts.hour[""],
+            }*/],
+        },
+        options: {
+            tooltips: {
+                displayColors: true,
+                callbacks:{
+                    mode: "x",
+                },
+            },
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                    type: "linear",
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: { position: "bottom" },
+        }
+    });
+}
 
-    var ctx = document.getElementById("menu1Chart").getContext('2d');
+function initializeRhDayChart() {
+    var ctx = document.getElementById("rh_day_canv").getContext("2d");
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["SUN","MON","TUE","WED","THU","FRI","SAT"],
+            datasets: [{
+                label: "Website",
+                backgroundColor: "#caf270",
+                data: rh_counts.day["Website"],
+            }, {
+                label: "Voice",
+                backgroundColor: "#45c490",
+                data: rh_counts.day["Voice"],
+            }/*, {
+                label: "Floor 1",
+                backgroundColor: "#008d93",
+                data: rh_counts.day[""],
+            }, {
+                label: "Floor 2",
+                backgroundColor: "#2e5468",
+                data: rh_counts.day[""],
+            }*/],
+        },
+        options: {
+            tooltips: {
+                displayColors: true,
+                callbacks:{
+                    mode: "x",
+                },
+            },
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                    type: "linear",
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: { position: "bottom" },
+        }
+    });
+}
 
-    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+function initializePieChart() {
+    var ctx = document.getElementById("pie_canv").getContext("2d");
+
+    var xValues = ["Website", "France", "Spain", "USA", "Argentina"];
     var yValues = [55, 49, 44, 24, 15];
     var barColors = [
         "#b91d47",
@@ -107,3 +303,41 @@ function create_charts(){
         }
     });
 }
+
+
+$("a[data-toggle='tab']").on("shown.bs.tab", function (e) {
+    var target = $(e.target).attr("href"); // Get the target tab
+
+    switch(target) {
+        case "#fh_time":
+            if (!$("#fh_time_canv").data("initialized")) {
+                initializeFhTimeChart();
+                $("#fh_time_canv").data("initialized", true);
+            }
+            break;
+        case "#fh_day":
+            if (!$("#fh_day_canv").data("initialized")) {
+                initializeFhDayChart();
+                $("#fh_day_canv").data("initialized", true);
+            }
+            break;
+        case "#rh_time":
+            if (!$("#rh_time_canv").data("initialized")) {
+                initializeRhTimeChart();
+                $("#rh_time_canv").data("initialized", true);
+            }
+            break;
+        case "#rh_day":
+            if (!$("#rh_day_canv").data("initialized")) {
+                initializeRhDayChart();
+                $("#rh_day_canv").data("initialized", true);
+            }
+            break;
+        case "#pie":
+            if (!$("#pie_canv").data("initialized")) {
+                initializePieChart();
+                $("#pie_canv").data("initialized", true);
+            }
+            break;
+    }
+});
